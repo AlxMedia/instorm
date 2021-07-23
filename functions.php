569c390361702cfe5dd095d8078bbb672d3b36b1
Alexander Agnarson <alexander.agnarson@gmail.com>
2021-07-23 18:45:54 +0200

    Disable support for widgets block editor, regen lang

--- functions.php
+++ functions.php
@@ -76,6 +76,9 @@ if ( ! function_exists( 'instorm_setup' ) ) {
 		// Enable support for selective refresh of widgets in customizer
 		add_theme_support( 'customize-selective-refresh-widgets' );
 		
+		// Disable support for widgets block editor
+		remove_theme_support( 'widgets-block-editor' );
+		
 		// Thumbnail sizes
 		add_image_size( 'instorm-small', 200, 200, true );
 		add_image_size( 'instorm-medium', 520, 293, true );
