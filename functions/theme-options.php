4d31a2a9cfa53b1e9eee6dc494f210a44cde2469
AlxMedia <contact@alx.media>
2023-08-04 13:10:59 +0200

    Update to Font Awesome 6.4.2

--- functions/theme-options.php
+++ functions/theme-options.php
@@ -584,7 +584,7 @@ Kirki::add_field( 'instorm_theme', array(
 	'label'			=> esc_html__( 'Create Social Links', 'instorm' ),
 	'description'	=> esc_html__( 'Create and organize your social links', 'instorm' ),
 	'section'		=> 'social',
-	'tooltip'		=> esc_html__( 'Font Awesome names:', 'instorm' ) . ' <a href="https://fontawesome.com/v5/search?s=brands" target="_blank"><strong>' . esc_html__( 'View All', 'instorm' ) . ' </strong></a>',
+	'tooltip'		=> esc_html__( 'Font Awesome names:', 'instorm' ) . ' <a href="https://fontawesome.com/search?o=r&m=free&f=brands" target="_blank"><strong>' . esc_html__( 'View All', 'instorm' ) . ' </strong></a>',
 	'row_label'		=> array(
 		'type'	=> 'text',
 		'value'	=> esc_html__('social link', 'instorm' ),
@@ -601,7 +601,7 @@ Kirki::add_field( 'instorm_theme', array(
 		'social-icon'	=> array(
 			'type'			=> 'text',
 			'label'			=> esc_html__( 'Icon Name', 'instorm' ),
-			'description'	=> esc_html__( 'Font Awesome icons. Ex: fa-facebook ', 'instorm' ) . ' <a href="https://fontawesome.com/v5/search?s=brands" target="_blank"><strong>' . esc_html__( 'View All', 'instorm' ) . ' </strong></a>',
+			'description'	=> esc_html__( 'Font Awesome icons. Ex: fa-facebook ', 'instorm' ) . ' <a href="https://fontawesome.com/search?o=r&m=free&f=brands" target="_blank"><strong>' . esc_html__( 'View All', 'instorm' ) . ' </strong></a>',
 			'default'		=> 'fa-',
 		),
 		'social-link'	=> array(
